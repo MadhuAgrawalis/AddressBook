155e17533e33e49449e6b1eaa6d132697081a2ed
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-02-02 15:17:08 +0530

    Uc3

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -13,8 +13,8 @@ namespace AddressBookSystem
             addTheDetails.ViewTheDetails();
             //addTheDetails.PrintTheDetails();
 
-            //UC3_EditDetails editDetails = new UC3_EditDetails();
-            //editDetails.EditUsingName();
+            UC3_EditDetails editDetails = new UC3_EditDetails();
+            editDetails.EditUsingName();
 
             //UC4_DeleteDetails deleteDetails = new UC4_DeleteDetails();
             //deleteDetails.DeletetheName();
